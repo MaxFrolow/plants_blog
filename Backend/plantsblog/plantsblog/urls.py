4312b63d4984c8f663db85b850c79c3804a17bcb
MaxFrolow <maxfrolow777@gmail.com>
2020-11-13 16:58:39 +0200

    Reset Backend

--- Backend/plantsblog/plantsblog/urls.py
+++ Backend/plantsblog/plantsblog/urls.py
@@ -1,4 +1,4 @@
-"""PlantsBlog URL Configuration
+"""plantsblog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import PostListView
-
-app_name = 'blogpost'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='postlist'),
+    path('admin/', admin.site.urls),
 ]
